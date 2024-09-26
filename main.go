f9fe6c556509093b775c2fa29394138597e71c10
rudev44 <ru.dev44@kgk-global.com>
2024-09-26 16:52:01 +0300

    nop

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"os"
 	"tcp-client/msg"
-	"time"
+	"tcp-client/pkg"
 )
 
 const (
@@ -16,9 +16,18 @@ const (
 	CONN_TYPE = "tcp"
 )
 
+type Packet struct {
+	Header  []byte
+	Length  []byte
+	Payload []byte
+	CRC     []byte
+}
+
 func main() {
 	conn, err := net.Dial(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
-	defer conn.Close()
+	defer func() {
+		_ = conn.Close()
+	}()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -26,9 +35,15 @@ func main() {
 
 	err = ReadNWrite(conn)
 	if err != nil {
-		log.Printf("Alarm, %s", err.Error())
+		log.Printf("Alarm ReadNWrite, %s", err.Error())
 	}
-
+	// time.Sleep(1 * time.Minute)
+	for {
+		err = listener(conn)
+		if err != nil {
+			log.Printf("Alarm listener, %s", err.Error())
+		}
+	}
 }
 
 func ReadNWrite(conn net.Conn) error {
@@ -36,44 +51,77 @@ func ReadNWrite(conn net.Conn) error {
 	msgs := msg.GetMsgs()
 
 	for i, msg := range msgs {
-		go func() {
-			fmt.Printf("Packet %d, %X\n", i, msg)
+		var p Packet
+		raw := buildPacket(i, p, msg)
+		fmt.Printf("RAW %X\n", raw)
 
-			_, write_err := conn.Write(msg)
-			if write_err != nil {
-				log.Printf("failed: %s", write_err.Error())
-			}
-			log.Println("Sent")
-			buf, read_err := io.ReadAll(conn)
-			if read_err != nil {
-				log.Printf("failed: %s", read_err.Error())
-			}
-			log.Println("Read out")
+		_, write_err := conn.Write(raw)
+		if write_err != nil {
+			return fmt.Errorf("failed WRITE: %w", write_err)
+		}
 
-			if len(buf) < 3 {
-				log.Printf("packet length error")
-			}
-			if buf[0] != 0x02 {
-				log.Printf("packet type  error")
-			}
+		buf := make([]byte, 3)
+		_, read_err := conn.Read(buf)
+		if read_err != nil {
+			return fmt.Errorf("failed READ: %w", read_err)
+		}
 
-			if getCRCfromBytes(msg) != getCRCfromBytes(buf) {
-				log.Printf("CRC error")
-			}
-			log.Println("CRC - ok")
-			// time.Sleep(1 * time.Second)}
-		}()
+		if len(buf) < 3 {
+			return fmt.Errorf("packet length error")
+		}
+		if buf[0] != 0x02 {
+			return fmt.Errorf("packet type  error")
+		}
+		if getCRCfromBytes(raw) != getCRCfromBytes(buf) {
+			return fmt.Errorf("CRC error")
+		}
+		log.Println("CRC - ok")
 	}
-	time.Sleep(210 * time.Second)
-
-	conn.(*net.TCPConn).CloseWrite()
 	return nil
 }
 
 func getCRCfromBytes(packet []byte) uint16 {
-	var crc uint16
-	crc = uint16(packet[len(packet)-2])
-	crc = crc << 8
-	crc = crc + uint16(packet[len(packet)-1])
+	crc := binary.LittleEndian.Uint16(packet[len(packet)-2:])
 	return crc
+}
+
+func buildPacket(i int, p Packet, msg []byte) []byte {
+	if i == 0 {
+		p.Header = append(p.Header, byte(0x01))
+	} else {
+		p.Header = append(p.Header, byte(0x08))
+	}
+	p.Payload = append(p.Payload, msg...)
+	p.Length = binary.LittleEndian.AppendUint16(p.Length, uint16(len(p.Payload)))
+
+	raw := append(p.Header, p.Length...)
+	raw = append(raw, p.Payload...)
+	p.CRC = binary.LittleEndian.AppendUint16(p.CRC, pkg.Crc16FFFF(raw))
+	raw = append(raw, p.CRC...)
+	return raw
+}
+
+func listener(conn net.Conn) error {
+	var (
+		localBuffer []byte
+		dataLen     int
+		err         error
+	)
+	readBuf := make([]byte, 1024)
+	if dataLen, err = conn.Read(readBuf); err != nil {
+		return fmt.Errorf("failed READ: %w", err)
+	} else if dataLen > 0 {
+		log.Printf("Incoming data siza = %d\n", dataLen)
+		localBuffer = append(localBuffer, readBuf[:dataLen]...)
+	}
+
+	if len(localBuffer) < 3 {
+		return fmt.Errorf("packet length error")
+	}
+	crc := pkg.Crc16FFFF(localBuffer[dataLen-2:])
+	if crc != getCRCfromBytes(localBuffer) {
+		return fmt.Errorf("CRC error")
+	}
+	log.Println("CRC - ok")
+	return nil
 }
